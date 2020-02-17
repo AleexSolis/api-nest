05999f37e6724819db56a1892ca37d060ccc9593
Alejandro Solis <aleexsolis@hotmail.com>
2020-02-17 14:52:08 -0600

    message

--- fronted/src/components/Dashboard.js
+++ fronted/src/components/Dashboard.js
@@ -151,7 +151,6 @@ export default function Dashboard() {
           Appointment saved
         </Alert>
       </Snackbar>
-      <Alert severity="success">This is a success message!</Alert>
     </Container>
   );
 }
