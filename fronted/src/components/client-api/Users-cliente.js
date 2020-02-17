52ca3350d1f461cf6206e2d56d499986226c5201
Alejandro Solis <aleexsolis@hotmail.com>
2020-02-17 11:05:32 -0600

    user state

--- fronted/src/components/client-api/Users-cliente.js
+++ fronted/src/components/client-api/Users-cliente.js
@@ -7,18 +7,18 @@ export const getSuperadmin = async () => {
 };
 
 export const createSuperadmin = async newUser => {
-  const user = await createUser(newUser, 1);
+  const user = await createUser(newUser, 1, 2);
   if (!user) return false;
   return user.data;
 };
 
-export const createUser = async (newUser, typeUser) => {
+export const createUser = async (newUser, typeUser, state = 1) => {
   const user = await axios.post(
     "http://localhost:3001/users/create",
     JSON.stringify({
       ...newUser,
       typeUser,
-      state: 2
+      state
     }),
     {
       headers: { "Content-Type": "application/json" }
